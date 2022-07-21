36273f07535ac7b833e620e45e8facf4c7536d35
Tom Duncalf <tom@tomduncalf.com>
2022-07-21 11:51:58 +0100

    Demo of type safe querying

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    '/Users/td/dev/realm/RealmTypesafeDemo/babel-plugin-realm-typesafe-queries',
+  ],
+};
---
+++ realm.d.ts
@@ -0,0 +1,28 @@
+declare namespace Realm {
+  type QueryableString = Pick<string, never> & {
+    startsWith: (search: string, caseInsensitive?: boolean) => boolean;
+    endsWith: (search: string, caseInsensitive?: boolean) => boolean;
+    contains: (search: string, caseInsensitive?: boolean) => boolean;
+    like: (s: string, caseInsensitive?: boolean) => boolean;
+  };
+
+  type QueryableArray<T> = Queryable<T> & {
+    count: () => number;
+    any: () => QueryableArray<T>;
+  };
+
+  type Queryable<T> = {
+    [P in keyof T]: T[P] extends string
+      ? QueryableString
+      : T[P] extends Array<any>
+      ? QueryableArray<T[P][0]>
+      : Queryable<T[P]>;
+  };
+
+  interface Collection<T> extends ReadonlyArray<T> {
+    filtered: (
+      fn: (x: Queryable<T>) => boolean,
+      deps?: () => any[],
+    ) => Results<T>;
+  }
+}
