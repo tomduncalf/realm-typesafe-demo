3e3541bc70427fda8ebfb56a75dd66738b241370
Tom Duncalf <tom@tomduncalf.com>
2022-07-22 14:48:08 +0100

    Use plugin from github

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    '/Users/td/dev/realm/RealmTypesafeDemo/babel-plugin-realm-typesafe-queries',
-  ],
+  plugins: ['babel-realm-typesafe-plugin'],
 };
